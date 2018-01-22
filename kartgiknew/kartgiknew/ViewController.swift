d237a5e48d6908d5c4b7340f2c26063ddce3a2d9
karthik <kasanikarthik@achiever-42.local>
2018-01-22 14:29:36 +0530

    new

---
+++ kartgiknew/kartgiknew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kartgiknew
+//
+//  Created by karthik on 1/22/18.
+//  Copyright © 2018 karthik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
